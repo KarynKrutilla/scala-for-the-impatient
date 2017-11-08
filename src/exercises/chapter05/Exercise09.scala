f76f921e0420c196f5c84dfbfdab41ef0bc1f026
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-08 14:02:34 -0800

    chapter 05 exercise 09 and 10

---
+++ src/exercises/chapter05/Exercise09.scala
@@ -0,0 +1,10 @@
+package exercises.chapter05
+
+/**
+ * 9. Reimplement the class of the preceding exercise in Java, C#, or C++ (your
+ *    choice). How much shorter is the Scala class?
+ */
+object Exercise09 extends App{
+  
+  
+}
---
+++ src/exercises/chapter05/Exercise10.scala
@@ -0,0 +1,16 @@
+package exercises.chapter05
+
+/**
+ * 10. Consider the class
+ *     class Employee(val name: String, var salary: Double) {
+ *       def this() { this("John Q. Public", 0.0) }
+ *     }
+ *     
+ *     Rewrite it to use explicit fields and a default primary constructor. Which form
+ *     do you prefer? Why?
+ * 
+ */
+object Exercise10 extends App {
+  
+  class Employee(val name: String = "John Q. Public", var salary: Double = 0.0) {}
+}
