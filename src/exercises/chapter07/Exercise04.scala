473071dd0fcc9f9bce33ac616c7d345c807cd5bd
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-14 10:13:58 -0800

    chapter 07 exercise 04

---
+++ src/exercises/chapter07/Exercise04.scala
@@ -0,0 +1,14 @@
+package exercises.chapter07
+
+/**
+ * 4. Why do you think the Scala language designers provided the package object
+ *    syntax instead of simply letting you add functions and variables to a package?
+ */
+object Exercise04 {
+  
+  /**
+   * From page 80:
+   * "A package can contain classes, objects, and traits, but not the definitions of
+   * functions or variables. That’s an unfortunate limitation of the Java virtual machine."
+   */
+}
