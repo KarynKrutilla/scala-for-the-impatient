2afe4dfff77cb8cc3c79a48a243c0266fa859410
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-14 09:21:22 -0800

    chapter 07 exercise 02

---
+++ src/exercises/chapter07/Exercise02.scala
@@ -0,0 +1,17 @@
+package exercises.chapter07
+
+/** 
+ *  2. Write a puzzler that baffles your Scala friends, using a package com that isn’t
+ *  	 at the top level.
+ */
+object Exercise02 extends App {
+  print(test.com.Test.name)
+  assert(test.com.Test.name == "test")
+}
+
+
+package test.com {
+  object Test {
+    val name = "test"
+  }
+}
