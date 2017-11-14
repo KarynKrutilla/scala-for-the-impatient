ad40833719b7e3b902e7c1e49393b90b40a06385
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-14 10:29:19 -0800

    chapter 07 exercise 05

---
+++ src/exercises/chapter07/Exercise05.scala
@@ -0,0 +1,12 @@
+package exercises.chapter07
+
+/**
+ * 5. What is the meaning of private[com] def giveRaise(rate: Double)? Is it useful?
+ */
+object Exercise05 {
+  
+  /**
+   * Limits the visibility to the com package
+   * It can be useful, but it does not affect much if everything is in the com package
+   */
+}
