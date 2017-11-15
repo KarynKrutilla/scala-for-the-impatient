372d615819ba3756e05bb4985e2ca9f68ab3b2ce
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-15 13:55:18 -0800

    chapter 07 exercise 07

---
+++ src/exercises/chapter07/Exercise07.scala
@@ -0,0 +1,26 @@
+package exercises.chapter07
+
+/**
+ * 7. In the preceding exercise, move all imports into the innermost scope possible.
+ */
+object Exercise07 extends App {
+  
+  // Init Java and Scala HashMaps
+  val javamap = new java.util.HashMap[String, String]
+  val scalamap = new collection.mutable.HashMap[String, String]
+  
+  javamap.put("0", "value0")
+  javamap.put("1", "value1")
+  
+  // Copy items from javamap to scalamap
+  val iter = javamap.entrySet().iterator()
+  while(iter.hasNext()) { 
+    val current = iter.next()
+    scalamap += (current.getKey -> current.getValue)
+  }
+  
+  // Test
+  assert(scalamap.get("0") == Some("value0"))
+  assert(scalamap.get("1") == Some("value1"))
+  scalamap.foreach(println)
+}
