f14c1e902aa9b66394a838f81b58bce1c1d25bcc
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-15 14:06:58 -0800

    chapter 07 exercise 08

---
+++ src/exercises/chapter07/Exercise08.scala
@@ -0,0 +1,15 @@
+package exercises.chapter07
+
+/**
+ * 8. What is the effect of
+ * 			import java._
+ * 			import javax._
+ * 		Is this a good idea?
+ */
+object Exercise08 {
+  
+  /**
+   * Imports all java classes under java and javax
+   * This includes far too many classes to be necessary in an import
+   */
+}
