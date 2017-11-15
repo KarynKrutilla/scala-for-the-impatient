3ed2f1bbe5c882cc39339c1561e3a1690ccbf21e
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-15 14:27:55 -0800

    chapter 07 exercise 10

---
+++ src/exercises/chapter07/Exercise10.scala
@@ -0,0 +1,18 @@
+package exercises.chapter07
+
+/**
+ * 10. Apart from StringBuilder, what other members of java.lang does the scala
+ * 		package override?
+ */
+object Exercise10 {
+  
+  /**
+   * Anything with a matching name will be overridden. 
+   * 
+   * From page 83:
+   * As with Java programs, java.lang is always imported. Next, the scala package
+   * is imported, but in a special way. Unlike all other imports, this one is allowed
+   * to override the preceding import.
+   * 
+   */
+}
