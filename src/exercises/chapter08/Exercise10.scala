75a0a94347d1130a18acaca1ab304c88812f869c
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-11-20 09:54:19 -0800

    chapter 08 exercise 10

---
+++ src/exercises/chapter08/Exercise10.scala
@@ -0,0 +1,16 @@
+package exercises.chapter08
+
+/**
+  * 10. The file scala/collection/immutable/Stack.scala contains the definition
+  *
+  *       class Stack[A] protected (protected val elems: List[A])
+  *
+  *     Explain the meanings of the protected keywords. (Hint: Review the discussion
+  *     of private constructors in Chapter 5.)
+  */
+object Exercise10 {
+
+  // Page 90:
+  // Protected keyword means that it is accessible from any subclass, but not from other locations.
+  
+}
