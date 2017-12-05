c54a41fcb4a1ede0edb715398e89f5b76cde81ee
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-12-05 13:42:02 -0800

    chapter 10 exercise 09

---
+++ src/exercises/chapter10/Exercise09.scala
@@ -0,0 +1,26 @@
+package exercises.chapter10
+
+import java.io.{BufferedInputStream, FileInputStream, InputStream}
+
+/**
+  * In the java.io library you can add buffering to an input stream with a BufferedInputStream decorator.
+  * Reimplement buffering as a trait. For simplicity, override the read method.
+  */
+object Exercise09 extends App {
+
+  // Reimplement buffering as a trait
+  trait Buffering {
+    this:InputStream =>
+      val buffer = new BufferedInputStream(this)
+      // For simplicity, override the read method
+      override def read(arr:Array[Byte]) = buffer.read(arr)
+  }
+
+  // Test
+  val input = new FileInputStream("src/exercises/chapter10/exercise09.txt") with Buffering
+  val arr = new Array[Byte](24)
+
+  input.read(arr)
+
+  arr.foreach(c => print(c.toChar))
+}
