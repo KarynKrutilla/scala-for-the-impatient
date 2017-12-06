9cd1f4c8b6bec09e6ac07e2e2a7d3ce14fcc62ee
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-12-06 14:54:28 -0800

    chapter 11 exercise 01

---
+++ src/exercises/chapter11/Exercise01.scala
@@ -0,0 +1,11 @@
+package exercises.chapter11
+
+/**
+  * According to the precedence rules, how are 3+4->5 and 3->4+5 evaluated?
+  */
+object Exercise01 {
+
+  // 3+4->5 == (3+4)->5
+  // 3->4+5 == (3->4)+5
+  
+}
