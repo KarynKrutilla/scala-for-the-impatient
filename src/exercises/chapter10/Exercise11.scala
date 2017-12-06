dd5200dc89f493c5e6653bd2d3869dde6e31235f
Karyn Krutilla <karyn.krutilla@gmail.com>
2017-12-06 09:53:39 -0800

    chapter 10 exercise 11

--- src/exercises/chapter10/Exercise11.scala
+++ src/exercises/chapter10/Exercise11.scala
@@ -1,5 +1,22 @@
 package exercises.chapter10
 
-object Exercise11 {
+import java.io.{BufferedInputStream, FileInputStream, InputStream}
+
+/**
+  * Implement a class IterableInputStream that extends java.io.InputStream with the trait Iterable[Byte]
+  */
+object Exercise11 extends App {
+
+  class IterableInputStream(val input: InputStream) extends InputStream with Iterable[Byte] {
+    def iterator = new Iterator[Byte] {
+      def hasNext() = input.available() > 0
+      def next() = input.read.toByte
+    }
+    def read = input.read()
+  }
+
+  // Test
+  val stream = new IterableInputStream(new FileInputStream("src/exercises/chapter10/chapter10.txt"))
+  stream.iterator.foreach(c => print(c.toChar))
 
 }
